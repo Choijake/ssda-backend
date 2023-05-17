467c358a0e82ac16d78862664b4a5abedf354104
Choijake <shameless8@naver.com>
2023-05-17 18:06:06 +0900

    SSDA-28 Feat: creat AccessToken

--- src/main/kotlin/com/boseyo/backend/jwt/AuthController.kt
+++ src/main/kotlin/com/boseyo/backend/jwt/AuthController.kt
@@ -1,4 +1,4 @@
-package com.boseyo.backend.controller
+package com.boseyo.backend.jwt
 
 import com.boseyo.backend.dto.LoginRequestDto
 import com.boseyo.backend.dto.TokenDto
@@ -7,6 +7,7 @@ import com.boseyo.backend.jwt.JwtTokenProvider
 import jakarta.validation.Valid
 import org.springframework.http.HttpHeaders
 import org.springframework.http.HttpStatus
+import org.springframework.http.ResponseCookie
 import org.springframework.http.ResponseEntity
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder
@@ -17,20 +18,38 @@ import org.springframework.web.bind.annotation.RequestBody
 import org.springframework.web.bind.annotation.RequestMapping
 import org.springframework.web.bind.annotation.RestController
 
+
 @RestController
 @RequestMapping("/api")
 class AuthController(
         private val tokenProvider: JwtTokenProvider,
         private val authenticationManagerBuilder: AuthenticationManagerBuilder
+
 ) {
     @PostMapping("/authenticate")
     fun authorize(@RequestBody @Valid loginDto: LoginRequestDto): ResponseEntity<TokenDto> {
         val authenticationToken = UsernamePasswordAuthenticationToken(loginDto.username, loginDto.password)
-        val authentication: Authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken)
+          val authentication: Authentication = authenticationManagerBuilder.getObject().authenticate(authenticationToken)
         SecurityContextHolder.getContext().setAuthentication(authentication)
         val jwt: String = tokenProvider.createToken(authentication)
         val httpHeaders = HttpHeaders()
         httpHeaders.add(JwtFilter.AUTHORIZATION_HEADER, "Bearer $jwt")
+
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+        val accessToken: String = tokenProvider.createToken(authentication)
+        // ResponseCookie 생성
+        val cookie = ResponseCookie.from("accessToken", accessToken)
+            .maxAge((7 * 24 * 60 * 60).toLong())
+            .path("/")
+            .secure(true)
+            .sameSite("None")
+            .httpOnly(true)
+            .build()
+        // 쿠키를 응답 헤더에 추가
+        val headers = HttpHeaders()
+        headers.add(HttpHeaders.SET_COOKIE, cookie.toString())
+////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
         return ResponseEntity<TokenDto>(TokenDto(jwt), httpHeaders, HttpStatus.OK)
     }
 }
